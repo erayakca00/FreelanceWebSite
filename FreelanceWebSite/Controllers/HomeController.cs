a1654b6c325dd8cd5d22b8f9afe82a26c153de62
erayakca00 <115736621+erayakca00@users.noreply.github.com>
2024-01-02 22:51:49 +0300

    Add project files.

---
+++ FreelanceWebSite/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using FreelanceWebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FreelanceWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()  
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Network()
+        {
+            return View();
+        }
+
+        public IActionResult Jobs()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
